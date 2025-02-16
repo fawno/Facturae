f31d71b08ef9d12dc0f1446b0195e95e01891f64
Fernando Herrero <ininflamable@gmail.com>
2025-02-16 17:02:50 +0100

    Nuevos métodos en DOMDocumentExtended

--- src/Signer/DOMDocumentExtended.php
+++ src/Signer/DOMDocumentExtended.php
@@ -14,17 +14,86 @@
 
   use DOMDocument;
   use DOMElement;
+  use DOMNode;
+  use Exception;
   use Fawno\Facturae\Facturae;
+  use Fawno\Facturae\FacturaeSigner;
+  use RobRichards\XMLSecLibs\XMLSecurityDSig;
 
   class DOMDocumentExtended extends DOMDocument {
+    public static function loadFile (string $filename, int $options = 0) : ?DOMDocumentExtended {
+      $dom = new static('1.0', 'utf-8');
+
+      return $dom->load($filename, $options) ? $dom : null;
+    }
+
+    /**
+     * Load XML from a string
+     * @param string $source The string containing the XML.
+     * @param int $options [optional] Bitwise OR of the libxml option constants.
+     * @return null|DOMDocumentExtended DOMDocumentExtended on success or null on failure.
+     */
+    public static function loadStringXML (string $source, int $options = 0) : ?DOMDocumentExtended {
+      $dom = new static('1.0', 'utf-8');
+
+      return $dom->loadXML($source, $options) ? $dom : null;
+    }
+
     public static function loadFacturae (Facturae $facturae) : ?DOMDocumentExtended {
-      $dom = new DOMDocumentExtended();
+      $dom = new DOMDocumentExtended('1.0', 'utf-8');
 
       return $dom->loadXML($facturae->asXML()) ? $dom : null;
     }
 
+    public function asDom () : DOMDocumentExtended {
+      return clone $this;
+    }
+
     public function asFacturae () : ?Facturae {
       return Facturae::importDOM($this);
+    }
+
+    public function isSigned () : ?bool {
+      $objXMLSecDSig = new XMLSecurityDSig();
+
+      $signature = $objXMLSecDSig->locateSignature($this);
+
+      return ($signature instanceof DOMNode);
+    }
+
+    public function removeSignature () : DOMDocumentExtended {
+      try {
+        $objXMLSecDSig = new XMLSecurityDSig();
+
+        $unsigned = $this->asDOM();
+
+        $signature = $objXMLSecDSig->locateSignature($unsigned);
+
+        if (!($signature instanceof DOMNode)) {
+          return $this;
+        }
+
+        $signature->parentNode->removeChild($signature);
+
+        $localName = array_search(FacturaeSigner::XMLNS_DS, $unsigned->getDocNamespaces());
+
+        if (is_string($localName)) {
+          $unsigned->documentElement->removeAttribute($localName);
+        }
+
+        return $unsigned;
+      } catch (Exception $exception) {
+        return $this;
+      }
+    }
+
+    public function setSellerEmail (string $email) : DOMDocumentExtended {
+      $ContactDetails = $this->getElementsByTagName('SellerParty')->item(0)->getElementsByTagName('ContactDetails')->item(0);
+      $ElectronicMail = $ContactDetails->getElementsByTagName('ElectronicMail');
+      $ElectronicMail = $ElectronicMail->length ? $$ElectronicMail->item(0) : $ContactDetails->appendChild($this->createElement('ElectronicMail'));
+      $ElectronicMail->nodeValue = 'distribucion@deia.eus';
+
+      return $this;
     }
 
     public function getDocumentAttributes () : array {
@@ -37,7 +106,7 @@
       return $attrs;
     }
 
-    public function getDocumentNamespaces (array $namespaces = []) : array {
+    public function getDocNamespaces (array $namespaces = []) : array {
       $xmlns = new self();
       $xmlns->loadXML($this->C14N());
       $xmlns = $xmlns->getDocumentAttributes();
