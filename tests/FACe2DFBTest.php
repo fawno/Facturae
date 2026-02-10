114efc1501f9d70ab5a58c1caf61fe22f2e32ee5
Fernando Herrero <ininflamable@gmail.com>
2026-02-10 13:43:11 +0100

    La DFB ha arreglado lo que tenía roto

--- tests/FACe2DFBTest.php
+++ tests/FACe2DFBTest.php
@@ -10,13 +10,6 @@
     public const ENDPOINT = 'http://80.245.2.226/CGS2/FacturaSSPPWebServiceProxyPort?WSDL';
     public const INVOICE = self::INVOICE_SAMPLE3;
     public const FCLASS = FACe2::class;
-    /**
-     * Debido a cambios de estructura en Diputación, no existe relación
-     * oficina contable-órgano gestor-unidad tramitadora en las tablas
-     * de órganos administrativos.
-     * Contacte con su enlace o consulte los nuevos códigos en la página web.
-     */
-    public const CODIGO1 = 'RCF08004';
 
     public function testEnviarFacturaRepetida () : void {
       $this->assertTrue(true);
