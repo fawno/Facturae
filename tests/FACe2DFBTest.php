baa6124994a6b7db0f1802582c85272728f95249
Fernando Herrero <ininflamable@gmail.com>
2026-03-02 13:16:23 +0100

    Actualizar fawno/face-relations

--- tests/FACe2DFBTest.php
+++ tests/FACe2DFBTest.php
@@ -11,6 +11,14 @@
     public const INVOICE = self::INVOICE_SAMPLE3;
     public const FCLASS = FACe2::class;
 
+    /**
+     * Debido a cambios de estructura en Diputación, no existe relación
+     * oficina contable-órgano gestor-unidad tramitadora en las tablas
+     * de órganos administrativos.
+     * Contacte con su enlace o consulte los nuevos códigos en la página web.
+     */
+    public const CODIGO1 = 'RCF08004';
+
     public function testEnviarFacturaRepetida () : void {
       $this->assertTrue(true);
       return;
